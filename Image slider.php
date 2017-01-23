326821f3db61ea41a39c7712a7fa19653439b091
lquick94 <quickl14@students.ecu.edu>
2017-01-23 08:52:38 -0500

    Added Logo and opacity to sections Signed-off-by: lquick94 <quickl14@students.ecu.edu>

---
+++ Image slider.php
@@ -0,0 +1,37 @@
+<div class="container">
+	<div id="slider">
+		<ul class="slides">
+			<li class="slide">
+				<img src="https://www.mylinea.com/wp-content/uploads/Beautiful-Sea-Sunset-Ultra-HD-Wallpaper-720x400.jpg" alt="Slide 1">
+			</li>
+			<li class="slide">
+				<img src="https://www.mylinea.com/wp-content/uploads/Beauty-Of-Nature-Wallpaper-720x400.jpg" alt="Slide 2">
+			</li>
+			<li class="slide">			
+				<img src="https://www.mylinea.com/wp-content/uploads/best-nature-wallpaper-53-720x400.jpg" alt="Slide 3">
+			</li>
+			<li class="slide">
+				<img src="https://www.mylinea.com/wp-content/uploads/amazing-nature-wallpaper-06-720x400.jpg" alt="Slide 4">
+			</li>
+			<li class="slide">
+				<img src="https://www.mylinea.com/wp-content/uploads/Beautiful-Sea-Sunset-Ultra-HD-Wallpaper-720x400.jpg" alt="Slide 1">
+			</li>				
+		</ul>
+	</div>
+	
+	<div class="clearfix"></div>
+	<div class="directions">
+		<span class="left"></span>
+	</div>
+	<div class="directions">
+		<span class="right"></span>
+	</div>
+		
+	<div class="bullets">
+		<span class="bullet active" id="bullet1" data-image="4" data-slide="1"></span>
+		<span class="bullet" id="bullet2" data-image="5" data-slide="2"></span>
+		<span class="bullet" id="bullet3" data-image="6" data-slide="3"></span>
+		<span class="bullet" id="bullet4" data-image="7" data-slide="4"></span>
+	</div>
+	
+</div>
--- overallHeader.php
+++ overallHeader.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<?php include 'head.html';?>
+	<div id = "logo">			
+    <h1><img src="logo.png"></img>Burgers &#8226; Pasta &#8226; Pizza</h1>
+	</div>
 	<?php include 'header.php';?>
 	
