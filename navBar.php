51f466d1d814d1716cc8e376bd13df9f034a051c
lquick94 <quickl14@students.ecu.edu>
2017-02-24 12:36:07 -0500

    Map added Signed-off-by: lquick94 <quickl14@students.ecu.edu>

--- navBar.php
+++ navBar.php
@@ -5,7 +5,7 @@
       <li><a href='#'><i class='fa fa-cutlery'></i><span>Menu</span></a></li>
       <li><a href='#'><i class='fa fa-user'></i><span>About</span></a></li>
       <li><a href='contact.php'><i class='fa fa-book'></i><span>Contact</span></a></li>
-      <li><a href='#'><i class='fa fa-map'></i><span>Locate</span></a></li>
+      <li><a href='locate.php'><i class='fa fa-map'></i><span>Locate</span></a></li>
     </ul>
   </div>
 	</div>
